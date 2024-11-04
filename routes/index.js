d7eb30fb4e6d3485a65f343cd2a0da5f8d6dcb14
thanhlt <163329784+thanhlt205@users.noreply.github.com>
2024-11-04 21:55:39 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Thế Thanh' });
+  res.render('index', { title: 'Thanh' });
 });
 
 module.exports = router;
